7c7fe4c4494ec50848d5b89867b55cc574de372e
Fengchuan-Song <1319825810@qq.com>
2026-05-19 02:27:28 +0000

    update during val

--- evalution.py
+++ evalution.py
@@ -126,7 +126,7 @@ if __name__ == "__main__":
     #   一般来讲，网络从0开始的训练效果会很差，因为权值太过随机，特征提取效果不明显，因此非常、非常、非常不建议大家从0开始训练！
     #   如果一定要从0开始，可以了解imagenet数据集，首先训练分类模型，获得网络的主干部分权值，分类模型的 主干部分 和该模型通用，基于此进行训练。
     #----------------------------------------------------------------------------------------------------------------------------#
-    model_path      = ""
+    model_path      = "/data/PSPNet/PSPNet/weights/best_epoch_weights_ep034_mIoU0.767.pth"
     #---------------------------------------------------------#
     #   downsample_factor   下采样的倍数8、16 
     #                       8下采样的倍数较小、理论上效果更好。
@@ -358,7 +358,7 @@ if __name__ == "__main__":
     #----------------------#
     if local_rank == 0:
         time_str        = datetime.datetime.strftime(datetime.datetime.now(),'%Y_%m_%d_%H_%M_%S')
-        log_dir         = os.path.join(save_dir, "loss_" + str(time_str))
+        log_dir         = os.path.join(save_dir, "eval_" + str(time_str))
         loss_history    = LossHistory(log_dir, model, input_shape=input_shape)
     else:
         loss_history    = None
@@ -397,7 +397,7 @@ if __name__ == "__main__":
     #---------------------------#
     #   读取数据集对应的txt
     #---------------------------#
-    with open(os.path.join(VOCdevkit_path, "MIPC_SemanticSegmentation/2007_val.txt"),"r") as f:
+    with open(os.path.join(VOCdevkit_path, "MIPC_SemanticSegmentation/2007_test.txt"),"r") as f:
         val_lines = f.readlines()
     num_val     = len(val_lines)
 
